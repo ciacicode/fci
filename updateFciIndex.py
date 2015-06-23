a075c38de6a11330907915df2259725741dd4ce7
Maria Cerase <maria.cerase@gmail.com>
2015-06-23 20:51:21 +0100

    implemented real indexing in the database and fci calculation, therefore the highest rate of fried cicken restaurants is now index 100 and so forth

--- updateFciIndex.py
+++ updateFciIndex.py
@@ -1,3 +1,4 @@
+
 '''
     script to update the fciIndex table
 
@@ -8,26 +9,45 @@
 
 
 import fciUtils
+import operator
 import pdb
 
-def updateFci():
-    '''updates the fciindex table '''
-    # connect to the database
+def find_max():
+    """returns highest fried chicken index"""
     db = fciUtils.connect_fci_db()
     cur = db.cursor()
-    # clear up the fciIndex table
-    cur.execute("TRUNCATE fciIndex")
-    db.commit()
-    # select postcodes from ordered_postcode
-    # pdb.set_trace()
     cur.execute("SELECT Postcode FROM ordered_postcodes")
     db.commit()
-    data = cur.fetchall()
-    data = sorted(set(data))
-    for value in data:
-        # calculate index
-        value = value[0]
-        fci = fciUtils.fci_calculate(value)
-        cur.execute("INSERT INTO fciIndex (Postcode,FCI) VALUES (%s,%s)", (value,fci))
+    postcodes = cur.fetchall()
+    postcodes =sorted(set(postcodes))
+    db.close()
+    #pdb.set_trace()
+    dict_fci = {}
+    for p in postcodes:
+        p = p[0]
+        fci = fciUtils.fci_calculate(p)
+        dict_fci[p] = fci
+    maximum_key = max(dict_fci.iteritems(), key=operator.itemgetter(1))[0]
+    maximum = dict_fci[maximum_key]
+    return maximum, dict_fci
+
+
+
+
+def updateFci(maximum, mydict):
+    '''
+    updates the fciindex table with indexes
+    accepts maximum raw fci and the fci list as input
+    '''
+
+    db = fciUtils.connect_fci_db()
+    cur = db.cursor()
+     # clear up the fciIndex table
+    cur.execute("TRUNCATE fciIndex")
+    db.commit()
+    for key, value in mydict.iteritems():
+        index = (value/maximum)*100
+        cur.execute("INSERT INTO fciIndex (Postcode,FCI) VALUES (%s,%s)", (key,index))
         db.commit()
+
     db.close()
